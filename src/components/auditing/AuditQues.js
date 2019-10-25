0b52474da6b842a7d030c77ca763cca0c743fd7c
Hasanga Lakdinu <hasangalakdinu@gmail.com>
2019-10-25 16:59:07 +0530

    add questions

--- src/components/auditing/AuditQues.js
+++ src/components/auditing/AuditQues.js
@@ -12,6 +12,10 @@ class AuditQues extends Component {
     return (
       
     <section className="container">
+    <h1 style={{textAlign:"center"}}>New Audit For ABC Product XyZ Technologies</h1>
+    <br/>
+    <br/>
+    <h6>WPC canvas gives you 16 model phases for evaluate your product, what you have to do is go through all the 16 models and answer the given questions</h6>
       {this.modelData.areas.map((area , i)=>{
          return <Specmodels key={i} area={area} />
        })}
